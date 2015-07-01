da1f845efe7f5000a7a0c8b45b3f7118d3c3a751
idan haviv <idan.haviv@gmail.com>
2015-07-01 17:19:01 +0300

    create project

---
+++ flickrDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  flickrDemo
+//
+//  Created by idan haviv on 7/1/15.
+//  Copyright (c) 2015 idan haviv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
